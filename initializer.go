7d2e5dcbc9779f32be0227c9d7bc8b545963fb18
devholic : MBPr (Mac) <devholic@plusquare.com>
2015-09-15 17:39:34 +0900

    add db connect

--- initializer.go
+++ initializer.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"database/sql"
+	"github.com/garyburd/redigo/redis"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func connectRedis() (c redis.Conn) {
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+	return c
+}
+
+func connectMysql() (c *sql.DB) {
+	db, err := sql.Open("mysql", "root:password@/dbname")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	return db
+}
+
+var redis_conn = connectRedis()
+var mysql_conn = connectMysql()
+
 func main() {
+
 }
